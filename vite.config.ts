bb742b25ab4124a82461db8492eb0e4bbaebb40e
Stephen Weiss <stephen.c.weiss@gmail.com>
2024-04-06 11:40:42 -0500

    feat: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
+import path from "path";
+
+import devServer, { defaultOptions } from "@hono/vite-dev-server";
 import { vitePlugin as remix } from "@remix-run/dev";
+import esbuild from "esbuild";
+import { routeExtensions }  from "remix-custom-routes";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import devServer from "@hono/vite-dev-server";
-import esbuild from "esbuild";
 
 export default defineConfig({
   server: {
@@ -11,39 +14,27 @@ export default defineConfig({
       key: "./server/dev/key.pem",
       cert: "./server/dev/cert.pem",
     },
-    // https://github.com/remix-run/remix/discussions/8917#discussioncomment-8640023
-    warmup: {
-      clientFiles: [
-        "./app/entry.client.tsx",
-        "./app/root.tsx",
-        "./app/routes/**/*",
-      ],
-    },
   },
-  // https://github.com/remix-run/remix/discussions/8917#discussioncomment-8640023
-  optimizeDeps: {
-    include: ["./app/routes/**/*"],
-  },
-  // Hello 👋 Prisma users, uncomment that. Thanks @AlemTuzlak
-  // resolve: {
-  //   alias: {
-  //     ".prisma/client/index-browser":
-  //       "./node_modules/.prisma/client/index-browser.js",
-  //   },
-  // },
   plugins: [
     devServer({
       injectClientScript: false,
       entry: "server/index.ts", // The file path of your server.
-      exclude: [/^\/(app)\/.+/, /^\/@.+$/, /^\/node_modules\/.*/],
+      exclude: [/^\/(app)\/.+/, ...defaultOptions.exclude],
     }),
     tsconfigPaths(),
     remix({
       serverBuildFile: "remix.js",
+      future: {
+        v3_fetcherPersist: true,
+      },
+      routes() {
+        // eslint-disable-next-line no-undef
+        const appDirectory = path.join(__dirname, "app");
+        return routeExtensions(appDirectory);
+      },
       buildEnd: async () => {
         await esbuild
           .build({
-            alias: { "~": "./app" },
             // The final file name
             outfile: "build/server/index.js",
             // Our server entry point
